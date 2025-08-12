c35850e118d334db626ef6c98dd3467de75a428a
Ali <ali.hounain@gmail.com>
2025-08-11 22:06:27 -0400

    new changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,12 +11,12 @@ export default function About() {
     >
       <h1 className="text-3xl font-bold mb-6 text-center">about me</h1>
       <p className="mb-4 my-auto  ">
-        My name is Ali Hounain and I&apos;m a senior Computer Science major at
+        My name is Ali Hounain and I&apos;m a recent Computer Science major grad from
         the University of Massachusetts Boston. I&apos;m passionate about software development
         and web technologies, constantly seeking to expand my knowledge and skills.
       </p>
       <p>
-        I&apos;m currently looking for internships in the fields of software development 
+        I&apos;m currently looking for new grad roles in the fields of software development 
         and web development. Feel free to explore my projects and learn more about 
         my interests and experiences.
       </p>
--- app/page.tsx
+++ app/page.tsx
@@ -5,26 +5,33 @@ import { Github, Linkedin } from 'lucide-react'
 
 export default function Home() {
   return (
-    <motion.div 
+    <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="flex flex-col items-center justify-center min-h-[calc(100vh-120px)]"
+      className='flex flex-col items-center justify-center min-h-[calc(100vh-120px)]'
     >
-      <h1 className="text-4xl font-bold mb-4">Ali Hounain</h1>
-      <p className="text-xl mb-8 text-center max-w-2xl">
-        Senior Computer Science major at the University of Massachusetts Boston. 
-        Aspiring software engineer.
+      <h1 className='text-4xl font-bold mb-4'>Ali Hounain</h1>
+      <p className='text-xl mb-8 text-center max-w-2xl'>
+        Recent Computer Science major graduate from the University of
+        Massachusetts Boston.
       </p>
-      <div className="flex space-x-4">
-        <Link href="https://github.com/ahounain" className="transition-transform hover:scale-110">
+      <p>Aspiring software engineer.</p>
+      <div className='flex space-x-4'>
+        <Link
+          href='https://github.com/ahounain'
+          className='transition-transform hover:scale-110'
+        >
           <Github size={24} />
         </Link>
-        <Link href="https://www.linkedin.com/in/ali-hounain/" className="transition-transform hover:scale-110">
+        <Link
+          href='https://www.linkedin.com/in/ali-hounain/'
+          className='transition-transform hover:scale-110'
+        >
           <Linkedin size={24} />
         </Link>
       </div>
     </motion.div>
-  )
+  );
 }
 
